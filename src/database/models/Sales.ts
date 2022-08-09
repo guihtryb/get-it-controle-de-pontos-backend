29d21d21ec4a4559eae61d1aad8cbb10ec05a42e
Guilherme Viana <guilhermehviana01@gmail.com>
2022-08-09 11:44:47 -0300

    chore(models): add sales' model

---
+++ src/database/models/Sales.ts
@@ -0,0 +1,77 @@
+import { DATE } from 'sequelize';
+import { Model, INTEGER, STRING, DECIMAL } from 'sequelize';
+import db from '.';
+import Users from './Users';
+
+class Sales extends Model {
+  id!: number;
+  userId: string;
+  sellerId: number;
+  totalPrice: number;
+  totalPoints: string;
+  deliveryAddress: string;
+  saleDate: Date;
+  status: string;
+}
+
+Sales.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userId: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      key: 'id',
+      model: 'users'
+    }
+  },
+  sellerId: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      key: 'id',
+      model: 'users'
+    }
+  },
+  totalPrice: {
+    type: DECIMAL(9, 2),
+    allowNull: false,
+  },
+  totalPoints: {
+    type: DECIMAL(9, 2),
+    allowNull: false,
+  },
+  deliveryAddress: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  deliveryNumber: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  saleDate: {
+    type: DATE,
+    allowNull: false,
+  },
+  status: {
+    type: STRING(100),
+    allowNull: false,
+  }
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'Sales',
+  timestamps: false
+});
+
+Sales.belongsTo(Users, { foreignKey: 'id', as: 'userId' });
+Sales.belongsTo(Users, { foreignKey: 'id', as: 'sellerId' });
+
+Users.hasMany(Sales, { foreignKey: 'id', as: 'userId'});
+Users.hasMany(Sales, { foreignKey: 'id', as: 'sellerId'});
+
+export default Sales;
