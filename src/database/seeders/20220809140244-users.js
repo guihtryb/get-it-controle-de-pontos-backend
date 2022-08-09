a6e5e19bb11f72de4d20ccf0d740619d23121c90
Guilherme Viana <guilhermehviana01@gmail.com>
2022-08-09 11:08:22 -0300

    chore(seeders): add users' seeder

---
+++ src/database/seeders/20220809140244-users.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          fullName: 'Get It Admin Jordan',
+          email: 'jordan@getit.com',
+          password: '$2a$12$NInfMuqnKFrxYxH6x3ZGd.vKM/BZi1X/cRvIOYBkLVdqVMzeFidwi',
+          role: 'adminsitrator',
+          points: 0,
+        },
+        {
+          fullName: 'Jhon Doe',
+          email: 'jhondoeshop@email.com',
+          password: '$2a$12$jXWtManRMG2hiPBygu3fD.1iILWGbKaL6Bqbc6qbpGPbz0S3LqWWS',
+          role: 'seller',
+          points: 0,
+        },
+        {
+          fullName: 'Eddie Vedder',
+          email: 'eddiev@email.com',
+          password: '$2a$12$uSgBEhVFYPrxFA5UL.nlzOmrZlPk6qn0PkPceS/bPPhJ1KxJyPdfu',
+          role: 'customer',
+          points: 1250,
+        },
+      ],
+      {},
+    );
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('users', null, {});
+  },
+};
