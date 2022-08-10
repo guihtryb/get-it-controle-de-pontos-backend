6d96dab20f92726ca13fdfe5e5863ad20ebe9248
Guilherme Viana <guilhermehviana01@gmail.com>
2022-08-09 23:06:12 -0300

    tests(services/users): add tests to users' service

---
+++ src/tests/mocks/userMock.ts
@@ -0,0 +1,29 @@
+import IUser from '../../interfaces/IUser';
+
+const userMock:IUser = {
+  fullName: 'Jhon Daltrey',
+  email: 'daltrey@email.com',
+  password: 'hashed password',
+  points: 1000,
+  role: 'customer',
+};
+
+const userMockWithId:IUser = {
+  id: 1,
+  fullName: 'Jhon Daltrey',
+  email: 'daltrey@email.com',
+  password: 'hashed password',
+  points: 1000,
+  role: 'customer'
+};
+
+const userMockWithIdUpdated:IUser = {
+  id: 1,
+  fullName: 'Jhon Daltrey',
+  email: 'daltrey@email.com',
+  password: 'hashed password',
+  points: 1800,
+  role: 'customer'
+};
+
+export { userMock, userMockWithId, userMockWithIdUpdated };
---
+++ src/tests/mocks/zod.ts
@@ -0,0 +1,11 @@
+export const zodError = [
+  {
+    code: "invalid_type",
+    expected: "string",
+    received: "undefined",
+    path: [
+      "model"
+    ],
+    message: "Required"
+  },
+];
