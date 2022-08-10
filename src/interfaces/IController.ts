c284721c3c24ff7d911eb02bb527e844a52c72d9
Guilherme Viana <guilhermehviana01@gmail.com>
2022-08-10 06:30:20 -0300

    build: nyc config file and IController*

---
+++ src/interfaces/IController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+
+export type ResponseError = {
+  error: unknown;
+};
+
+export interface RequestWithBody<T> extends Request {
+  body: T;
+}
+
+export default interface Controller<T> {
+  create(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+
+  getAll(
+    req: Request,
+    res: Response<T[] | ResponseError>,
+  ): Promise<typeof res>;
+
+  getById(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+
+  update(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+
+  delete(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+}
